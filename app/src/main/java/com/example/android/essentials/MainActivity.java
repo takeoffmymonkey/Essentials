d4603cee387189ef7cbadf13def69a9754ec69c5
takeoff <galukhin@gmail.com>
2017-07-18 14:09:03 +0300

    test webview

--- app/src/main/java/com/example/android/essentials/MainActivity.java
+++ app/src/main/java/com/example/android/essentials/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.essentials;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.test_webv);
+
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Log.e("WARNING: ", "No sd card");
+        } else {
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),
+                    "Essentials/princ.htm");
+            if (file.exists()) {
+                webView.loadUrl("file://" + Environment.getExternalStorageDirectory()
+                        + "/Essentials/princ.htm");
+            } else Log.e("WARNING: ", "File not found");
+
+        }
+
     }
 }
