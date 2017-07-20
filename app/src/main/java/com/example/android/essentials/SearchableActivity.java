fa1bcc343d1e6a49426238ba69a1c6699745b108
takeoff <galukhin@gmail.com>
2017-07-20 17:47:04 +0300

    search

--- app/src/main/java/com/example/android/essentials/SearchableActivity.java
+++ app/src/main/java/com/example/android/essentials/SearchableActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by takeoff on 020 20 Jul 17.
@@ -16,12 +17,26 @@ public class SearchableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        handleIntent();
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent();
+
+    }
+
+    public void handleIntent() {
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            TextView tv = (TextView) findViewById(R.id.search_query);
+            tv.setText(query);
+
             //doMySearch(query);
         }
     }
-
 }
